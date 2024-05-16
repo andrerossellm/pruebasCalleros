ee5e832e66bf402fb9d2b7852e287ed3a4671751
Manuel Zepeda del Río <manuel_zepeda@Manuels-MacBook-Pro.local>
2024-05-15 20:19:01 -0600

    Ejemplo 3

--- jasmine-standalone-4.6.0/spec/main.js
+++ jasmine-standalone-4.6.0/spec/main.js
@@ -12,5 +12,17 @@ describe(`${User.name} Class`, () => {
 
   describe("additional matchers examples", () => {
     // toBeDefined(), toEqual()
+    it("gets full name pieces", () => {
+      //arrange
+      const firstName = "Dylan";
+      const middleName = "Christopher";
+      const lastName = "Israel";
+
+      // act
+      model = new User({ firstName, middleName, lastName });
+
+      //assert
+      expect(model.fullNamePieces).toEqual([firstName, middleName, lastName]);
+    });
   });
 });
