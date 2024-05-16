c4a2548fdd7f8bc84adce536ac48227832471034
André Rossell <92490116+andrerossellm@users.noreply.github.com>
2024-05-15 18:01:31 -0600

    prueba 2 paso 1

--- jasmine-standalone-4.6.0/spec/main.js
+++ jasmine-standalone-4.6.0/spec/main.js
@@ -1,6 +1,22 @@
-// Majority
-describe('Test Example', () => {
-	it('returns true', () => {
-		expect(false).toBe(true); 
-	});
+function insertDashes(str) {
+  // write code here
+}
+
+/**
+ Test Suite
+*/
+describe("insertDashes()", () => {
+  it("insert dashes in between chars", () => {
+    // arrange
+    const str = "aba caba";
+
+    // act
+    const result = insertDashes(str);
+
+    // log
+    console.log("result: ", result);
+
+    // assert
+    expect(result).toBe("a-b-a c-a-b-a");
+  });
 });
