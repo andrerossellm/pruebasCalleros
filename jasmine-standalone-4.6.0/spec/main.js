bb723c7e24696fd6fac584b5bea13e220e320b72
Manuel Zepeda del Río <manuel_zepeda@Manuels-MacBook-Pro.local>
2024-05-15 19:39:34 -0600

    Prueba 5

--- jasmine-standalone-4.6.0/spec/main.js
+++ jasmine-standalone-4.6.0/spec/main.js
@@ -1,21 +1,23 @@
-describe (`${User.name} Class`, () => { 
+describe(`${User.name} Class`, () => {
   let model;
 
   beforeEach(() => {
     model = new User();
-  })
+  });
 
-  describe( 'default values', () => {
-    it('first name defaults to empty', () => {
-    expect(model.firstName).toBe('');
-    });
+  describe("say my name", () => {
+    it("alerts the full name of user", () => {
+      // arange
+      model.firstName = "Dylan";
+      model.lastName = "Israel";
+      spyOn(window, "alert");
 
-    it('last name defaults to empty', () => {
-    expect(model.lastName).toBe('');
-    });
+      // act
+      model.sayMyName();
 
-    it('middle name defaults to empty', () => {
-    expect(model.middleName).toBe('');
+      // assert
+      expect(window.alert).toHaveBeenCalled();
+      expect(window.alert).toHaveBeenCalledWith("Dylan Israel");
     });
   });
 });
